26298bca2daefb1079ad610057fdd73703e0f2b9
Clayton Brown <claytonlb2@gmail.com>
2016-09-16 19:40:22 -0400

    cleaned up css and added css for endorsement page

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['sass']);
     gulp.watch('assets/js/**', ['jekyll-rebuild']);
-		gulp.watch(['index.html', 'meetcyndi.html', 'issues.html', 'news.html', 'volunteer.html', 'assets/img/*', '_data/*', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
+		gulp.watch(['index.html', 'meetcyndi.html', 'issues.html', 'news.html', 'volunteer.html', 'endorsements.html', 'assets/img/*', '_data/*', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
 });
 
 /**
